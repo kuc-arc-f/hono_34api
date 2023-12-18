43f2935c69abf5b75537ad2ab913f0494e47d517
_ñ <knaka0209@gmail.com>
2023-12-19 08:25:37 +0900

    up, 1219a2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
-
+import { cors } from 'hono/cors'
 //routes
 import testRouter from './routes/test';
 //
 const app = new Hono()
+app.use("/*", cors());
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
